22946a35e8f2c902b7efc229ae90a04d27a94d23
Maksim Denisov <pss_pto@bk.ru>
2022-07-10 00:08:57 +0500

    Getting started with c#

---
+++ BasicCS_DML_09.07.2022/001/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+//комментарии
+
+/*
+Console.WriteLine("Hello, World!");
+System.Console.WriteLine(11%3);
+
+int MaxFromToNumbers=0;
+double d=3.14;
+System.Console.WriteLine(int.MaxValue);
+Console.WriteLine(double.MaxValue);
+string s="Hello";//конкатенация строк (склеивание)
+char c='a';
+*/
+
+/*
+int a=100;
+int b;
+b=a*a;
+System.Console.WriteLine("{0}^2={1}",a,b);//строка форматирования
+System.Console.WriteLine($"{a}^2={b}");//строка интерполяции
+*/
+
+
+/*string? s;//объявили
+s=Console.ReadLine();//присвоили
+
+System.Console.WriteLine(s);
+*/
+
+double a=3.14;
+double b;
+string? s=Console.ReadLine();//ввод данных
+a=Convert.ToDouble(s);
+
+
+b=a*a;//обработка данных
+
+//вывод результата
+System.Console.WriteLine("{0}^2={1}",a,b);//строка форматирования
+System.Console.WriteLine($"{a}^2={b}");//строка интерполяции
+
