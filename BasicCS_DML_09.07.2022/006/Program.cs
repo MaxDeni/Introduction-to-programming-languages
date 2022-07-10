618e18e5d4f21280879afca9dd0410a425f7f579
Maksim Denisov <pss_pto@bk.ru>
2022-07-10 16:40:48 +0500

    006

---
+++ BasicCS_DML_09.07.2022/006/Program.cs
@@ -0,0 +1,34 @@
+﻿//Написать программу вычисления значения функции y = sin(a). (Класс Math)
+/*double x,y;
+//x=3.1415;
+x=Math.PI;
+y=Math.Sin(x);
+System.Console.WriteLine($"sin({x})={y}");
+
+double r=1.0/3;
+System.Console.WriteLine(r);
+*/
+
+//тип возвращаемого значения Имя_фунции(аргументы функции)
+double Sin(double x)//методы
+{
+    return Math.Sin(x);
+}
+
+double Sum(double a, double b)
+{
+    return a+b;
+}
+
+
+void Pause()//метод
+{
+    System.Console.WriteLine("Press any key");
+}
+
+double y=Sin(3.14);
+System.Console.WriteLine(y);
+Pause();
+//System.Console.WriteLine(Pause());
+
+
