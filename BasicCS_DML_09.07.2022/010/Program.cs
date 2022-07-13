6de498377cd266c9b3027c2220e42dffe21290ca
Maksim Denisov <pss_pto@bk.ru>
2022-07-13 10:49:06 +0500

    Task 010 дополнил решение

--- BasicCS_DML_09.07.2022/010/Program.cs
+++ BasicCS_DML_09.07.2022/010/Program.cs
@@ -1,4 +1,14 @@
 ﻿//Вывести на экран последнюю цифру целого числа введенного с клавиатуры.
+
+/*
 int a=5018932;
+int b=a%10;
+System.Console.WriteLine(b);
+*/
+
+int a;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+
 int b=a%10;
 System.Console.WriteLine(b);
