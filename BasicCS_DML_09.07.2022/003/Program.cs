bd4d5ed5584ef04815a86e7166041a2dab5648a4
Maksim Denisov <pss_pto@bk.ru>
2022-07-13 17:49:29 +0500

    13.07.2022

---
+++ BasicCS_DML_09.07.2022/003/Program.cs
@@ -0,0 +1,22 @@
+﻿//С клавиатуры вводятся два числа a и b. Найти максимальное из них.
+int a,b;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+
+if (a>b)
+{
+    System.Console.WriteLine("a является максимальным числом");
+}
+else
+{
+    if (b>a)
+    {
+        System.Console.WriteLine("b является максимальным числом");
+    }
+    else
+    {
+        System.Console.WriteLine("Числа a и b являются равнозначными");
+    }
+}
---
+++ BasicCS_DML_09.07.2022/007/Program.cs
@@ -0,0 +1,15 @@
+﻿//Выяснить является ли число чётным.
+
+int a;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+
+
+if(a%2==0)
+{
+    System.Console.WriteLine("число является чётным");
+}
+else
+{
+    System.Console.WriteLine("число является не чётным");
+}
