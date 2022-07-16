8e7c599ceaafd6c62cda4f8513abf8fdb1f8f9dc
Maksim Denisov <pss_pto@bk.ru>
2022-07-17 00:39:04 +0500

    Tasks

--- BasicCS_DML_09.07.2022/Variables/Program.cs
+++ BasicCS_DML_09.07.2022/Variables/Program.cs
@@ -1,12 +1,13 @@
-﻿//.NET
-//значисые типы (value types)
+﻿
+//.NET
+//значимые типы (value types)
 int a;
 double b;
 a=4;
 //ссылочные типы (reference types)
 string s;
 s="Hello";
-string s1
+string s1;
 s1=s;//копирует
 int[] arr;
 arr=new int[50];
@@ -14,5 +15,4 @@ arr[0]=1;
 int[] arr2;
 arr2=arr;
 arr2[0]=2;
-
 System.Console.WriteLine();
