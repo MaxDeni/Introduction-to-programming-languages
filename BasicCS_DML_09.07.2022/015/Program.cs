2bc598d4073f19ec05326cd2057152d3aa414ad8
Maksim Denisov <pss_pto@bk.ru>
2022-07-17 12:47:13 +0500

    Tasks

---
+++ BasicCS_DML_09.07.2022/015/Program.cs
@@ -0,0 +1,13 @@
+﻿//С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет.
+
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2)
+{
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else 
+{
+  Console.WriteLine("-> третьей цифры нет");
+}
