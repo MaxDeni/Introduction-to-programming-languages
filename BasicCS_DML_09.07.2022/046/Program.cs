52faa5e8637a44b6c4ae6b9f7d6d9e00673ec888
Maksim Denisov <pss_pto@bk.ru>
2022-07-23 18:07:35 +0500

    Tasks

---
+++ BasicCS_DML_09.07.2022/046/Program.cs
@@ -0,0 +1,24 @@
+﻿//46. Показать числа Фибоначчи
+/*
+int fibonacchi;
+{
+    t=new int
+    {
+        void fibonacchi(int n)
+        {
+            if (n<3)
+                return 1;
+            else
+                return fibonacchi(n-2)+fibonacchi(n-1);
+        }
+        static void Main(string[] args)
+        {
+            for (int i=1; i<20; i++)
+            {
+                System.Console.WriteLine(fibonacchi(i));
+            }
+            System.Console.ReadKey();
+        }
+    }
+}
+*/
