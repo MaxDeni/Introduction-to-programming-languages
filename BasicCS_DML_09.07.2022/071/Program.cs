7ea43256fc3ba4e9e2bb87d3ea3efd9773c55fe8
Maksim Denisov <pss_pto@bk.ru>
2022-08-19 01:18:51 +0500

    Tasks

---
+++ BasicCS_DML_09.07.2022/071/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 71*. Написать программу вычисления функции Аккермана
+
+static UInt32 A(uint m, uint n)
+
+{
+if (m==0)
+    return n+1;
+else 
+    if ((m!=0) && (n==0))
+        return A(m-1,1);
+    else
+        return A(m-1, A(m, n-1));
+}
+
+System.Console.WriteLine(A(3,1));
