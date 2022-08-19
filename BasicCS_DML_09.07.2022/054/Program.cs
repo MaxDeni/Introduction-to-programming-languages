3a018f75bdef607eafe13a13fd60f23b55a06e57
Maksim Denisov <pss_pto@bk.ru>
2022-08-19 23:20:07 +0500

    Tasks

---
+++ BasicCS_DML_09.07.2022/054/Program.cs
@@ -0,0 +1,60 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+/*
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+int N=3;
+int M=4;
+
+int [,] a;
+
+a=new int [N,M];
+
+Random random=new Random();
+
+for(int i=0;i<a.GetLength(0);i++)
+{
+    for(int j=0;j<a.GetLength(1);j++) 
+    {
+        a[i,j]=random.Next(1,10);
+    }
+}
+
+for(int i=0;i<a.GetLength(0);i++)
+{
+    for(int j=0;j<a.GetLength(1);j++) System.Console.Write($"{a[i,j]} ");
+         System.Console.WriteLine();
+}
+
+System.Console.WriteLine();
+
+for(int i=0;i<a.GetLength(0);i++)
+{
+    for(int j=0;j<a.GetLength(1)-1;j++)
+    {
+        for(int z=0;z<a.GetLength(1)-1;z++)
+        {
+            if(a[i,z]<a[i,z+1])
+            {
+                int temp=0;
+                temp=a[i,z];
+                a[i,z]=a[i,z+1];
+                a[i,z+1]=temp;
+            }
+        }
+    }
+}
+
+for(int i=0;i<a.GetLength(0);i++)
+{
+    for(int j=0;j<a.GetLength(1);j++) System.Console.Write($"{a[i,j]} ");
+         System.Console.WriteLine();
+}
---
+++ BasicCS_DML_09.07.2022/056/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+/*
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+int N=4;
+int M=4;
+
+int [,] a;
+
+a=new int [N,M];
+
+Random random=new Random();
+
+for(int i=0;i<a.GetLength(0);i++)
+{
+    for(int j=0;j<a.GetLength(1);j++) 
+    {
+        a[i,j]=random.Next(1,10);
+    }
+}
+
+for(int i=0;i<a.GetLength(0);i++)
+{
+    for(int j=0;j<a.GetLength(1);j++) System.Console.Write($"{a[i,j]} ");
+         System.Console.WriteLine();
+}
+
+System.Console.WriteLine();
+
+int minsum=Int32.MaxValue;
+int indexLine=0;
+
+for(int i=0;i<a.GetLength(0);i++)
+{
+    int sum=0;
+    for (int j=0;j<a.GetLength(1);j++)
+    {
+        sum=sum+a[i,j];        
+    }
+    if (sum<minsum)
+    {
+        minsum=sum;
+        indexLine++;
+    }
+}
+
+Console.WriteLine("Cтрока " + (indexLine));
